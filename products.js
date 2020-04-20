1fd1a84119aaf2b05f3f950b507a63c50cfe8b29
Karol <56819265+lilivin@users.noreply.github.com>
2020-04-20 19:36:51 +0200

    Add files via upload

--- products.js
+++ products.js
@@ -1,42 +1,42 @@
 let products = [ 
     {
-        "brand": 'nike',
+        "brand": 'Nike',
         "model": 'model',
         "description": 'opis',
         "price": 99.99
     },
     {
-        "brand": 'nike',
+        "brand": 'Nike',
         "model": 'model',
         "description": 'opis',
         "price": 99.99
     },
     {
-        "brand": 'nike',
+        "brand": 'Nike',
         "model": 'model',
         "description": 'opis',
         "price": 99.99
     },
     {
-        "brand": 'nike',
+        "brand": 'Nike',
         "model": 'model',
         "description": 'opis',
         "price": 99.99
     },
     {
-        "brand": 'adidas',
+        "brand": 'Adidas',
         "model": 'model',
         "description": 'opis',
         "price": 99.99
     },
     {
-        "brand": 'adidas',
+        "brand": 'Adidas',
         "model": 'model',
         "description": 'opis',
         "price": 99.99
     },
     {
-        "brand": 'adidas',
+        "brand": 'Adidas',
         "model": 'model',
         "description": 'opis',
         "price": 99.99
@@ -44,8 +44,16 @@ let products = [
 ]
 
 let productsContainer = document.getElementById('productsContainer');
+let filterButton = document.getElementById('filterButton');
+let brandsList = document.getElementById('brandsList');
+let resetFilterButton = document.getElementById('resetFilterButton');
+let brandsListUserChoose = brandsList.options[brandsList.selectedIndex].value;
+
+
 
 function addProducts() {
+    brandsList.selectedIndex = 0;
+    productsContainer.innerHTML= '';
     let product = document.createElement("div");
     product.className = 'col-md-4 my-2';
     for(let i=0; i <= products.length-1; i++) {
@@ -71,6 +79,45 @@ function addProducts() {
     }
 }
 
+function filterProducts() {
+    productsContainer.innerHTML = '';
+    let product = document.createElement("div");
+    product.className = 'col-md-4 my-2';
+        for(let i=0; i <= products.length-1; i++) {
+            if(products[i].brand == brandsListUserChoose) {
+                let product = document.createElement("div");
+                product.className = 'col-md-4 my-2';
+                product.innerHTML = `
+                <div class="card mb-4 h-100">
+                    <img src="images/kiks.jpg" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <span class="badge badge-success px-4 py-2">New!</span>
+                            <h3 class="card-title mb-0">
+                                ${products[i].brand} ${products[i].model}
+                            </h3>
+                            <p class="mt-0">${products[i].description}</p>
+                        </div>
+                    <div class="card-footer text-center">
+                        ${products[i].price}$
+                    </div>
+                </div>
+                `
+                console.log(product);
+                productsContainer.appendChild(product);
+            } else {
+                console.log('brak')
+            }
+        }
+}
+
 
 
 addProducts();
+
+filterButton.addEventListener('click', function(e){
+    e.preventDefault();
+    brandsListUserChoose = brandsList.options[brandsList.selectedIndex].value;
+    filterProducts();
+})
+
+resetFilterButton.addEventListener('click', addProducts);
