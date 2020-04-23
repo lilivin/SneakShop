fb46604d2d1fd81fa62609376ee22d4eef7c822b
Karol <56819265+lilivin@users.noreply.github.com>
2020-04-23 22:50:34 +0200

    Add files via upload

--- products.js
+++ products.js
@@ -41,10 +41,41 @@ let products = [
         "description": 'opis',
         "price": 89.99
     },
+    {
+        "brand": 'Adidas',
+        "model": 'model',
+        "description": 'opis',
+        "price": 89.99
+    },
+    {
+        "brand": 'Adidas',
+        "model": 'model',
+        "description": 'opis',
+        "price": 89.99
+    },
+    {
+        "brand": 'Adidas',
+        "model": 'model',
+        "description": 'opis',
+        "price": 89.99
+    },
+    {
+        "brand": 'Adidas',
+        "model": 'model',
+        "description": 'opis',
+        "price": 89.99
+    },
+    {
+        "brand": 'Adidas',
+        "model": 'model',
+        "description": 'opis',
+        "price": 89.99
+    }
 ]
 
 let productsContainer = document.getElementById('productsContainer');
 let filterButton = document.getElementById('filterButton');
+let moreProductsButton = document.getElementById('moreProductsButton');
 let brandsList = document.getElementById('brandsList');
 let resetFilterButton = document.getElementById('resetFilterButton');
 let brandsListUserChoose = brandsList.options[brandsList.selectedIndex].value;
@@ -54,13 +85,41 @@ products.forEach(function(product){
     pricesArray .push(product.price);
 })
 
+function showAllProducts() {
+        brandsList.selectedIndex = 0;
+        moreProductsButton.style.display = 'none';
+        productsContainer.innerHTML= '';
+        let product = document.createElement("div");
+        product.className = 'col-md-4 my-2';
+        for(let i=0; i < products.length; i++) {
+            let product = document.createElement("div");
+            product.className = 'col-md-4 my-2';
+            product.innerHTML = `
+            <div class="card mb-4 h-100">
+                <img src="images/kiks.jpg" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <span class="badge badge-success px-4 py-2">New!</span>
+                        <h3 class="card-title mb-0">
+                            ${products[i].brand} ${products[i].model}
+                        </h3>
+                        <p class="mt-0">${products[i].description}</p>
+                    </div>
+                <div class="card-footer text-center">
+                    ${products[i].price}$
+                </div>
+            </div>
+            `
+            productsContainer.appendChild(product);
+        }
+}
 
 function addProducts() {
     brandsList.selectedIndex = 0;
+    moreProductsButton.style.display = 'block';
     productsContainer.innerHTML= '';
     let product = document.createElement("div");
     product.className = 'col-md-4 my-2';
-    for(let i=0; i <= products.length-1; i++) {
+    for(let i=0; i < 9; i++) {
         let product = document.createElement("div");
         product.className = 'col-md-4 my-2';
         product.innerHTML = `
@@ -78,84 +137,18 @@ function addProducts() {
             </div>
         </div>
         `
-        console.log(product);
         productsContainer.appendChild(product);
     }
 }
 
-/*
-
-function filterProductsBrand() {
-    let product = document.createElement("div");
-    product.className = 'col-md-4 my-2';
-        for(let i=0; i <= products.length-1; i++) {
-            if(products[i].brand == brandsListUserChoose) {
-                let product = document.createElement("div");
-                product.className = 'col-md-4 my-2';
-                product.innerHTML = `
-                <div class="card mb-4 h-100">
-                    <img src="images/kiks.jpg" alt="" class="card-img-top">
-                        <div class="card-body">
-                            <span class="badge badge-success px-4 py-2">New!</span>
-                            <h3 class="card-title mb-0">
-                                ${products[i].brand} ${products[i].model}
-                            </h3>
-                            <p class="mt-0">${products[i].description}</p>
-                        </div>
-                    <div class="card-footer text-center">
-                        ${products[i].price}$
-                    </div>
-                </div>
-                `
-                console.log(product);
-                productsContainer.appendChild(product);
-            } else {
-                return
-            }
-        }
-}
-
-function filterProductsPrice() {
-    
-    for(let z=0; z < products.length; z++) {
-        console.log(products[z].price);
-        console.log(z);
-        if(products[z].price < priceOfProduct) {
-            let product = document.createElement("div");
-            product.className = 'col-md-4 my-2';
-            product.innerHTML = `
-            <div class="card mb-4 h-100">
-                <img src="images/kiks.jpg" alt="" class="card-img-top">
-                    <div class="card-body">
-                        <span class="badge badge-success px-4 py-2">New!</span>
-                        <h3 class="card-title mb-0">
-                            ${products[z].brand} ${products[z].model}
-                        </h3>
-                        <p class="mt-0">${products[z].description}</p>
-                    </div>
-                <div class="card-footer text-center">
-                    ${products[z].price}$
-                </div>
-            </div>
-            `
-            console.log(product);
-            productsContainer.appendChild(product);
-        } else {
-            console.log('cos');
-        }
-    }
-}
-
-*/
+addProducts();
 
 let productsMapPrice = [];
 let productsMapBrand = [];
 let productsFinal = [];
 
 function filterPrice() {
-    //let priceOfProduct = document.getElementById('priceOfProduct').value;
     if(document.getElementById('maxPriceOfProduct').value != "" && document.getElementById('minPriceOfProduct').value == "") {
-        console.log("min");
         for(let q = 0; q < products.length; q++) {
             if(products[q].price < parseInt(document.getElementById('maxPriceOfProduct').value)) {
                 productsMapPrice.push(products[q]);
@@ -163,7 +156,6 @@ function filterPrice() {
         }
         filterBrand();
     } else if(document.getElementById('maxPriceOfProduct').value == "" && document.getElementById('minPriceOfProduct').value != "") {
-        console.log("max");
         for(let q = 0; q < products.length; q++) {
             if(products[q].price > parseInt(document.getElementById('minPriceOfProduct').value)) {
                 productsMapPrice.push(products[q]);
@@ -171,7 +163,6 @@ function filterPrice() {
         }
         filterBrand();
     } else if(document.getElementById('maxPriceOfProduct').value != "" && document.getElementById('minPriceOfProduct').value != "") {
-        console.log("oba");
         for(let q = 0; q < products.length; q++) {
             if((parseInt(document.getElementById('minPriceOfProduct').value) < products[q].price) && (products[q].price < parseInt(document.getElementById('maxPriceOfProduct').value))) {
                 productsMapPrice.push(products[q]);
@@ -189,31 +180,39 @@ function filterBrand() {
         for(let q = 0; q < productsMapPrice.length; q++) {
             if(productsMapPrice[q].brand == brandsList.options[brandsList.selectedIndex].value) {
                 productsMapBrand.push(productsMapPrice[q]);
-                console.log('push')
             }
         }
+        sort();
     } else {
         productsMapBrand = [...productsMapPrice];
+        sort();
     }
 }
 
 function sort() {
-    let sortType = document.querySelector('input[name="sortTypes"]:checked').value;
-    productsFinal = [...productsMapBrand];
-    if(sortType == 'up') {
-        productsFinal.price.sort(function(a, b){return a-b});
-    } else if (sortType == 'down') {
-        productsFinal.sort(function(a, b){return b-a});
+    let sortType = document.querySelector('input[name="sortTypes"]:checked');
+    if (sortType != null) {
+        if(sortType.value == 'up') {
+            productsFinal = productsMapBrand.slice(0);
+            productsFinal.sort(function(a,b) {
+                return a.price - b.price;
+            });
+        } else if (sortType.value == 'down') {
+            productsFinal = productsMapBrand.slice(0);
+            productsFinal.sort(function(a,b) {
+                return b.price - a.price;
+            });
+        }
+        draw();
+    } else {
+        productsFinal = [...productsMapBrand];
+        draw();
     }
 }
-
-addProducts();
 
 function draw() {
     productsContainer.innerHTML = [];
     for(let z = 0; z < productsFinal.length; z++) {
-        console.log(z);
-        console.log(productsFinal);
     let productContainer = document.createElement("div");
         productContainer.className = 'col-md-4 my-2';
         productContainer.innerHTML = `
@@ -233,18 +232,24 @@ function draw() {
         `
         productsContainer.appendChild(productContainer);
     }
+        console.log(productsFinal.length);
+        console.log(products.length);
+        moreProductsButton.style.display = 'none';
+    clearArrays();
+}
+
+function clearArrays() {
+    productsMapPrice = [];
+    productsMapBrand = [];
+    productsFinal = [];
 }
 
 filterButton.addEventListener('click', function(e){
     e.preventDefault();
     productsContainer.innerHTML = '';
-    //brandsListUserChoose = brandsList.options[brandsList.selectedIndex].value;
-    //filterProductsBrand();
-    //filterProductsPrice();
     filterPrice();
-    filterBrand();
-    //sort();
-    draw();
 })
+
+moreProductsButton.addEventListener('click', showAllProducts);
 
 resetFilterButton.addEventListener('click', addProducts);
